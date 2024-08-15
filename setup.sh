9a1397e1626a9fcbf2365a40f7bc65a1a554f8ae
Changyo Han <k9s1j7@gmail.com>
2024-08-15 22:27:01 +0900

    add further patches and install scripts

--- setup.sh
+++ setup.sh
@@ -30,32 +30,56 @@ python3.11 -m pip install -U \
       nose pep8 psutil pydocstyle pydot pygraphviz pyparsing==2.4.7 \
       pytest-mock rosdep rosdistro setuptools==59.6.0 vcstool
 
-# clone ros2_m1_native
-git clone https://github.com/TakanoTaiga/ros2_m1_native.git
-mkdir ${HOME}/ros2_m1_native/src
-cd ${HOME}/ros2_m1_native/
+# clone ros2_humble_m1
+git clone https://github.com/hanchangyo/ros2_humble_m1
+mkdir ${HOME}/ros2_humble_m1/src
+cd ${HOME}/ros2_humble_m1/
 
 patch -l < patches/ros2_console_bridge_vendor.patch
 patch -l < patches/ros2_rviz_ogre_vendor.patch
+patch -l < patches/ros2_visualization_rqt_bag.patch
+patch -l < patches/ros2_pybind11_vendor.patch
 
-# build ros2_m1_native
+# build ros2_humble_m1
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$(brew --prefix qt@5)
 export PATH=$PATH:$(brew --prefix qt@5)/bin
 export COLCON_EXTENSION_BLOCKLIST=colcon_core.event_handler.desktop_notification
-python3.11 -m colcon build --symlink-install --cmake-args \
+
+# Get the Python executable path
+export PYTHON_EXECUTABLE=$(which python)
+echo "Python executable: $PYTHON_EXECUTABLE"
+
+# Get the Python include directory
+export PYTHON_INCLUDE_DIR=$(python -c "from sysconfig import get_paths; print(get_paths()['include'])")
+echo "Python include directory: $PYTHON_INCLUDE_DIR"
+
+# Get the Python library directory
+export PYTHON_LIB_DIR=$(python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+echo "Python library directory: $PYTHON_LIB_DIR"
+
+# Get the actual Python library file (e.g., libpython3.11.dylib)
+export PYTHON_LIBRARY_FILE=$(python -c "import sysconfig; print(sysconfig.get_config_var('LDLIBRARY'))")
+export PYTHON_LIBRARY="$PYTHON_LIB_DIR/$PYTHON_LIBRARY_FILE"
+echo "Python library: $PYTHON_LIBRARY"
+
+# Export necessary environment variables for CMake
+export CMAKE_LIBRARY_PATH="$PYTHON_LIB_DIR:$CMAKE_LIBRARY_PATH"
+
+python3.11 -m colcon build --symlink-install --packages-skip-by-dep qt_gui_cpp --packages-skip qt_gui_cpp --cmake-args \
             -DBUILD_TESTING=OFF \
             -DTHIRDPARTY=FORCE \
             -DCMAKE_BUILD_TYPE=Release \
+            -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE" \
+            -DPYTHON_INCLUDE_DIR="$PYTHON_INCLUDE_DIR" \
+            -DPYTHON_LIBRARY="$PYTHON_LIBRARY" \
+            "$@" \
             -Wno-dev
 
+echo "source ${HOME}/ros2_humble_m1/install/setup.zsh" >> ${HOME}/.zshrc
+echo "export ROS_VERSION=2" >> ${HOME}/.zshrc
+echo "export ROS_PYTHON_VERSION=3" >> ${HOME}/.zshrc
+echo "export ROS_DISTRO=humble" >> ${HOME}/.zshrc
+echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ${HOME}/.zshrc
 
-echo "source ~/ros2_m1_native/install/setup.zsh" >> ~/.zshrc
-echo "export ROS_VERSION=2" >> ~/.zshrc
-echo "export ROS_PYTHON_VERSION=3" >> ~/.zshrc
-echo "export ROS_DISTRO=humble" >> ~/.zshrc
 
-
-source ~/ros2_m1_native/install/setup.zsh
-export ROS_VERSION=2
-export ROS_PYTHON_VERSION=3
-export ROS_DISTRO=humble
+source ${HOME}/.zshrc
